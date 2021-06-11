21cc4da7d74f0f4182e1eb5fd12e0db0dc52e38f
maciasmr22 <85133157+maciasmr22@users.noreply.github.com>
2021-06-11 00:17:03 -0500

    Update Videoteca.h

--- Videoteca.h
+++ Videoteca.h
@@ -1,1 +1,25 @@
+#include "Serie.h"
+#include "Pelicula.h"
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Videoteca{
+    protected:
+    Video **inventario;
+    int tamano;
+    int tamano_mem;
+
+    public:
+    Videoteca();
+    Videoteca(int);
+    Videoteca( Video **, int );
+
+    int getTamano();
+    Video ** getInventario();
+
+    void setInventario( Video **, int);
+
+    void agregarVideo(int, Video *);
+    void enlistar();
+};

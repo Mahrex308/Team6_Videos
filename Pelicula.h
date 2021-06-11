e1026d03636793239a3cbed778fc9c05dd55847e
AndreaCorona523 <85129680+AndreaCorona523@users.noreply.github.com>
2021-06-10 20:33:59 -0500

    Update Pelicula.h

--- Pelicula.h
+++ Pelicula.h
@@ -1,1 +1,24 @@
 #include <string>
+using namespace std;
+
+#ifndef VIDEO_H
+#define VIDEO_H
+#include "Video.h"
+#endif
+
+class Pelicula : public Video
+{
+    protected:
+    string clasificacion;
+
+    public:
+    Pelicula();
+    Pelicula(int, string, string, int, int, string, float = 0);
+
+    string getClasificacion();
+
+    void setClasificacion(string);
+    
+    void muestraDatos();
+
+};

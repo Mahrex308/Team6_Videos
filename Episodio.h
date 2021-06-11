c3b345fc616feb2c094617de7383e23127286ce9
Marino <marinohk6@gmail.com>
2021-06-10 23:18:36 -0500

    Update Episodio.h
    Se subio "Episodio.h"

--- Episodio.h
+++ Episodio.h
@@ -1,1 +1,24 @@
+#include <string>
+using namespace std;
 
+
+class Episodio{
+
+public:
+    string nom_episodio;
+    int num_episodio; /////// punto y coma
+
+public:
+    Episodio();
+    Episodio(string,int);
+
+    string getNom_episodio();
+    int getNum_episodio();
+    
+    void setNom_episodio(string);
+    void setNum_episodio(int); 
+    
+    void print();
+
+
+};
